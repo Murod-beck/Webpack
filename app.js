6ffc8e9f51e583a1d7a0be7a05051772c71dd6a6
Murod-beck <murodbeck.off@gmail.com>
2023-08-08 14:26:20 +0300

    local

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 const bodys = document.querySelector("body");
 const text = document.querySelector("h1");
-const inputs = document.querySelector(".form-check-input");
+const input = document.querySelector(".form-check-input");
 const icons = document.querySelector(".bi");
 const labels = document.querySelector(".form-check-label");
 const card = document.querySelector(".card");
 
 //Dark Light
-inputs.addEventListener("click", () => {
+const modeLocal = localStorage.getItem("mode");
+
+if (modeLocal) {
+  localMode();
+  input.setAttribute("checked", "checked");
+}
+
+function localMode() {
   card.classList.toggle("cards");
   text.classList.toggle("texts");
   bodys.classList.toggle("bodys");
   icons.classList.toggle("bi-moon-fill");
   if (labels.textContent === "Light") {
     labels.textContent = "Dark";
+    localStorage.setItem("mode", "dark");
   } else {
     labels.textContent = "Light";
+    localStorage.setItem("mode", "");
   }
+}
+
+input.addEventListener("click", () => {
+  localMode();
 });
